2f23d751efb6ce58191814990cc31025a3cba4bf
Mohd Ajlal <ajlal00786@gmail.com>
2025-01-31 00:57:19 +0530

    Home page and use-debounce

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,18 +1,54 @@
+import { Card, FeaturedCard } from "@/components/Cards";
+import Search from "@/components/Search";
+import icons from "@/constants/icons";
+import images from "@/constants/images";
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Image, SafeAreaView, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
+  // Function to get current time in HH:MM format
+  const getTime = () => {
+    const date = new Date();
+    const hours = date.getHours();
+    const minutes = date.getMinutes().toString().padStart(2, "0");
+    return `${hours}:${minutes}`;
+  };
+
+  // Function to get appropriate greeting
+  const getGreeting = () => {
+    const hours = new Date().getHours();
+    if (hours < 12) return "Good Morning!";
+    if (hours < 18) return "Good Afternoon!";
+    return "Good Evening!";
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="font-bold my-10 font-rubik text-3xl">Welcome To Restate</Text>
+    <SafeAreaView className="bg-white h-full">
+      <View className="px-5">
+        <View className="flex flex-row items-center justify-between mt-5">
+          <View className="flex flex-row">
+            <Image source={images.avatar} className="size-12 rounded-full" />
+            <View className="flex flex-col items-start ml-2 justify-center">
+              <Text className="text-xs font-rubik text-black-100">{getGreeting()}</Text>
+              <Text className="text-base font-rubik-medium text-black-300">Mohd Ajlal</Text>
+            </View>
+          </View>
 
+          <Image source={icons.bell} className="size-6"/>
+        </View>
+      <Search/>
+      <View className="my-5">
+        <View className="flex flex-row items-center justify-between">
+          <Text className="text-xl font-rubik-bold text-black-300">Featured</Text>
+          <TouchableOpacity>
+            <Text className="text-base font-rubik-bold text-primary-300">See All</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      </View>
 
-    </View>
+      <FeaturedCard/>
+      <Card/>
+    </SafeAreaView>
   );
 }
---
+++ components/Cards.tsx
@@ -0,0 +1,19 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export const FeaturedCard = () => {
+  return (
+    <View>
+      <Text>FeaturedCard</Text>
+    </View>
+  )
+}
+
+export const Card = () => {
+    return (
+      <View>
+        <Text>Card</Text>
+      </View>
+    )
+  }
+  
