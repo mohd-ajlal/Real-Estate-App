28191c22ad28ee5657354ea2c8ac550eac19de90
Mohd Ajlal <ajlal00786@gmail.com>
2025-02-03 00:53:02 +0530

    Implement Home Screen UI

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,4 +1,5 @@
 import { Card, FeaturedCard } from "@/components/Cards";
+import Filters from "@/components/Filters";
 import Search from "@/components/Search";
 import icons from "@/constants/icons";
 import images from "@/constants/images";
@@ -44,9 +45,32 @@ export default function Index() {
             <Text className="text-base font-rubik-bold text-primary-300">See All</Text>
           </TouchableOpacity>
         </View>
-      </View>
+
+        <View className="flex flex-row gap-5 mt-5">
+
       <FeaturedCard/>
-      <Card/>
+      <FeaturedCard/>
+      <FeaturedCard/>
+
+
+        </View>
+      </View>
+
+      <View className="flex flex-row items-center justify-between">
+          <Text className="text-xl font-rubik-bold text-black-300">Our Recommendation </Text>
+          <TouchableOpacity>
+            <Text className="text-base font-rubik-bold text-primary-300">See All</Text>
+          </TouchableOpacity>
+        </View>
+
+      <Filters/>
+        <View className="flex flex-row gap-5 mt-5">
+          <Card/>
+          <Card/>
+
+        </View>
+      {/* <FeaturedCard/>
+      <Card/> */}
       </View>
 
     </SafeAreaView>
